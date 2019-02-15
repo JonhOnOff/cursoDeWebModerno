de59a2f531dc2f3cc2b86dcb490327d888d426c0
Jonathan <jonathan.onoff@gmail.com>
2019-02-15 09:40:56 -0300

    Adição do arquivo tipajemfraca.js

---
+++ 00 Javascript: fundamentos/tipajenfraca.js
@@ -0,0 +1,13 @@
+let qualquer = "legal"
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nome generico e sigla
+
+let valor = ' '
+let numero = 1
+let pqp = false //Produto quimico perigosi ... Kkkkk
