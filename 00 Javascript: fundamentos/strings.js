ed401c08a7e7e9010b5069147209e42f18f904a6
Jonathan <jonathan.onoff@gmail.com>
2019-02-20 20:48:34 -0300

    Adicionamos o arquivo string.js, colocamos um novo exemplo de concatenação no arquivo nueroslagunscuidados.js

---
+++ 00 Javascript: fundamentos/strings.js
@@ -0,0 +1,11 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('r'))
+console.log(escola.substr(1))
+console.log(escola.substr(0, 3))
+console.log("escola ".concat(escola).concat("!"))
+console.log(escola.replace('o', 'e'))
+console.log("Ana, Maria, Pedro".split(","))
